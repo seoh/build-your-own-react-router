a89e53e64c201ad1f06278c4acd86075df516b48
Seoh Char <devthewild@gmail.com>
2017-04-02 21:42:24 +0900

    implement `route/Link.js`

--- src/router/Link.js
+++ src/router/Link.js
@@ -1,9 +1,39 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+
+const historyPush = (path) => {
+  history.pushState({}, null, path)
+}
+
+const historyReplace = (path) => {
+  history.replaceState({}, null, path)
+}
 
 export default class Link extends Component {
+  constructor (props) {
+    super(props)
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick (event) {
+    const { replace, to } = this.props
+    event.preventDefault()
+
+    replace ? historyReplace(to) : historyPush(to)
+  }
+
   render () {
+    const { to, children } = this.props
+
     return (
-      <span>link {this.props.to}</span>
+      <a href={to} onClick={this.handleClick}>
+        { children }
+      </a>
     )
   }
+}
+
+Link.propTypes = {
+  to: PropTypes.string.isRequired,
+  replace: PropTypes.bool
 }
