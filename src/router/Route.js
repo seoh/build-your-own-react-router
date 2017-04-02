630bbbbf897f8ea1ab2005b971757f511b147876
Seoh Char <devthewild@gmail.com>
2017-04-02 21:28:43 +0900

    implement `router/Route.js` without ES7 features

--- src/router/Route.js
+++ src/router/Route.js
@@ -1,9 +1,71 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+
+const matchPath = (pathname, options) => {
+  const { exact = false, path } = options
+
+  if (!path) {
+    return {
+      path: null,
+      url: pathname,
+      isExact: true
+    }
+  }
+
+  const match = new RegExp(`^${path}`).exec(pathname)
+
+  if (!match) {
+    return null
+  }
+
+  const url = match[0]
+  const isExact = pathname === url
+
+  if (exact && !isExact) {
+    return null
+  }
+
+  return { path, url, isExact }
+}
 
 export default class Route extends Component {
-  render () {
-    return (
-      <span>route [{this.props.path}] </span>
-    )
+  constructor (props) {
+    super(props)
+
+    this.handlePop = this.handlePop.bind(this)
   }
+
+  componentWillMount () {
+    addEventListener('popstate', this.handlePop)
+  }
+
+  componentWillUnmount () {
+    removeEventListener('popstate', this.handlePop)
+  }
+
+  handlePop () {
+    this.forceUpdate()
+  }
+
+  render () {
+    const { path, exact, component, render } = this.props
+    const match = matchPath(location.pathname, { path, exact })
+
+    if (!match) {
+      return null
+    }
+    if (component) {
+      return React.createComponent(component, { match })
+    }
+    if (render) {
+      return render({ match })
+    }
+    return null
+  }
+}
+
+Route.propTypes = {
+  exact: PropTypes.bool,
+  path: PropTypes.string,
+  Component: PropTypes.func,
+  render: PropTypes.func
 }
